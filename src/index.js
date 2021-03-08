7208490b941f6077398d7051bfd5ac97487fad3b
Sergei M <61053189+SergeiM63@users.noreply.github.com>
2021-03-08 22:21:13 +0400

    fix: add work code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+  if (number === 0) return 'zero';
+
+  let hundredCount = Math.floor(number / 100);
+  let tenCount = Math.floor((number - hundredCount * 100) / 10);
+  let oneCount = number - hundredCount * 100 - tenCount * 10;
+
+  if (tenCount === 1) {
+    tenCount = 0;
+    oneCount = number - hundredCount * 100;
+  }
+
+  const onesReadableString = [
+    '', 'one', 'two', 'three', 'four', 'five', 'six', 'seven',
+    'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen',
+    'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'
+  ];
+  const tensReadableString = [
+    '', '', 'twenty ', 'thirty ', 'forty ', 'fifty ', 'sixty ',
+    'seventy ', 'eighty ', 'ninety '
+  ];
+
+  const hundredReadableString = hundredCount ? onesReadableString[hundredCount] + ' hundred ' : '';
+    return (hundredReadableString + tensReadableString[tenCount] + onesReadableString[oneCount]).trim()
+};
